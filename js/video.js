c7058af1ae6a8b37952828ac10c82a875324539c
Ashik Mia <ashikmia5686@gmail.com>
2024-10-09 10:46:45 +0600

    navbar code

---
+++ js/video.js
@@ -0,0 +1,10 @@
+console.log('hollo world')
+
+// category function 
+
+const categoryApi = fetch('https://openapi.programming-hero.com/api/phero-tube/categories')
+
+
+const LoadCategory = () => {
+    
+}
